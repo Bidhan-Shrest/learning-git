f79669bfae0968d2a67e382d1831b71a9f043c2e
Bidhan-Shrest <xthabidhan57@gmail.com>
2023-12-19 17:49:09 +0545

    var, let and const

---
+++ inner.js
@@ -0,0 +1,25 @@
+
+// var person = 54321
+
+
+// // function details=[
+// //     name: "bidhan"
+// //     age: 23
+// //     state: "alone"
+// // ]
+// // console.log("details")
+// var person = "bidhan"
+// person =  222
+// console.log(person)
+
+// let age = 23
+// let name = "bidhan"
+// name = 20
+
+// console.log(age, name)
+
+const name = "bidhan"
+nam = 222
+
+console.log(name)
+
